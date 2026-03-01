1253e0ea2eed320ae62c8cadf3f88c90a48233fa
tr7032-ui <tr7032@srmist.edu.in>
2026-03-01 22:08:25 +0530

    Add files via upload

---
+++ Oopsone.java
@@ -0,0 +1,46 @@
+/**
+ * UC2 - Print OOPS Banner using String Formatting
+ * @author Tanishka
+ * @version 1.0
+ */
+public class Oopsone {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join("   ",
+                " *** ",
+                " *** ",
+                " ****",
+                " ****");
+
+        String line2 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*    ");
+
+        String line3 = String.join("   ",
+                "*   *",
+                "*   *",
+                " ****",
+                " ****");
+
+        String line4 = String.join("   ",
+                "*   *",
+                "*   *",
+                "*    ",
+                "    *");
+
+        String line5 = String.join("   ",
+                " *** ",
+                " *** ",
+                "*    ",
+                " ****");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
